6c574e86798663410bb6059b64718ae22e518642
Ahmedtaha <Ahmedbinaltaha@gmail.com>
2026-06-15 21:23:19 +0300

    updating config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,6 +9,9 @@ class settings(BaseSettings):
     FILE_ALLOWED_TYPES: list
     MAX_FILE_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+    
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
 
     class Config:
         env_file = 'src/.env'
